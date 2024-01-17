428fed16ccc07839ed8940f84bcc58caa8c631af
DEENUU1 <kacperwlodarczyk@protonmail.com>
2024-01-18 00:31:01 +0100

    Add dark/light mode

--- jarvis/src/app/layout.tsx
+++ jarvis/src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
-import './globals.css'
+import {ThemeProvider} from "./ThemeProvier"
+import {ThemeSwitcher} from "@/app/ThemeSwitch";
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -16,7 +17,14 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <body
+        className={`${inter.className} bg-slate-50 dark:bg-[#0d1117]`}
+    >
+    <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+        <ThemeSwitcher/>
+        <main>{children}</main>
+    </ThemeProvider>
+    </body>
     </html>
   )
 }
--- jarvis/tailwind.config.ts
+++ jarvis/tailwind.config.ts
@@ -6,15 +6,10 @@ const config: Config = {
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
+  darkMode: ["class"],
   theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
-    },
+    extend: {},
   },
   plugins: [],
-}
+};
 export default config
