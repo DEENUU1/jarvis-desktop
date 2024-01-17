fe0ef338f7c02c9c51e977cf3296278b26bbd46f
DEENUU1 <kacperwlodarczyk@protonmail.com>
2024-01-18 00:10:48 +0100

    Init tauri and nextjs project

--- jarvis/preload.js
+++
@@ -1,6 +0,0 @@
-const { contextBridge, ipcRenderer } = require('electron/renderer')
-
-contextBridge.exposeInMainWorld('darkMode', {
-    toggle: () => ipcRenderer.invoke('dark-mode:toggle'),
-    system: () => ipcRenderer.invoke('dark-mode:system')
-})
--- jarvis/renderer.js
+++
@@ -1,4 +0,0 @@
-document.getElementById('toggle-dark-mode').addEventListener('click', async () => {
-    const isDarkMode = await window.darkMode.toggle()
-    document.getElementById('theme-source').innerHTML = isDarkMode ? 'Dark' : 'Light'
-})
