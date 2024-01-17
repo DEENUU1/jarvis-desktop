f64e06e2ebcc3e6694ed549225d15cf77ed3f14a
DEENUU1 <kacperwlodarczyk@protonmail.com>
2024-01-17 23:29:57 +0100

    Add dark/light mode to electron app

--- jarvis/main.js
+++ jarvis/main.js
@@ -1,13 +1,30 @@
-const { app, BrowserWindow } = require('electron/main')
+const { app, BrowserWindow, ipcMain, nativeTheme } = require('electron/main')
+const path = require('node:path')
 
-const createWindow = () => {
+function createWindow () {
     const win = new BrowserWindow({
         width: 800,
-        height: 600
+        height: 600,
+        webPreferences: {
+            preload: path.join(__dirname, 'preload.js')
+        }
     })
 
     win.loadFile('index.html')
 }
+
+ipcMain.handle('dark-mode:toggle', () => {
+    if (nativeTheme.shouldUseDarkColors) {
+        nativeTheme.themeSource = 'light'
+    } else {
+        nativeTheme.themeSource = 'dark'
+    }
+    return nativeTheme.shouldUseDarkColors
+})
+
+ipcMain.handle('dark-mode:system', () => {
+    nativeTheme.themeSource = 'system'
+})
 
 app.whenReady().then(() => {
     createWindow()
