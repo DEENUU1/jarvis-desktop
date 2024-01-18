16eaad2b4b54eb766756609e1e3761a8bceb98f8
DEENUU1 <kacperwlodarczyk@protonmail.com>
2024-01-18 13:57:38 +0100

    Fetch all messages with useEffect

--- jarvis/next.config.js
+++ jarvis/next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export',
+    "env": {
+        "API_URL": process.env.API_URL
+    }
 }
 
 module.exports = nextConfig
--- jarvis/src/components/Chat.tsx
+++ jarvis/src/components/Chat.tsx
@@ -1,13 +1,20 @@
-import {getConversationMessages} from "@/lib/chat";
+'use client'
+import {useState, useEffect} from "react";
 
+export default function Chat({sessionId}: {sessionId: string}) {
+    const [data, setData] = useState()
 
-export default async function Chat({sessionId}: {sessionId: string}) {
-    const data = await getConversationMessages(sessionId)
+    useEffect(() => {
+        fetch("http://16.171.185.186" + `/chat/${sessionId}`)
+            .then(res => res.json())
+            .then(data => setData(data))
+            .catch(err => console.log(err))
+    })
 
     return (
         <>
             <div className="max-w-lg mx-auto my-8 p-4 border rounded-md shadow-lg">
-                {data.map((message, index) => (
+                {data?.map((message: any, index: number) => (
                     <div
                         key={index}
                         className={`mb-4 p-4 rounded-md`}
