78f71c06bcf6b6140616979be07f813e11c6f94e
DEENUU1 <kacperwlodarczyk@protonmail.com>
2024-01-19 15:02:33 +0100

    Reverse list of conversations

--- jarvis/src/components/SideBar.tsx
+++ jarvis/src/components/SideBar.tsx
@@ -34,7 +34,7 @@ export default async function Sidebar() {
             </SheetHeader>
 
             <div>
-                {data.session_ids.map((conversationId: string) => (
+                {data.session_ids.reverse().map((conversationId: string) => (
                     <Link href={`/${conversationId}`} key={conversationId}>
                         <h2 className="mb-2 border-2 rounded-2xl p-2 hover:bg-blue-100 border-gray-500">{conversationId}</h2>
                     </Link>
